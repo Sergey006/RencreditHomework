3bced787a2a9c18dafb2e6a89973a9a238a84bff
Sergey <sergeygreblov@gmail.com>
2020-02-14 22:43:33 +0300

    add MainSteps

---
+++ src/main/java/ru/aplana/autotests/steps/MainSteps.java
@@ -0,0 +1,22 @@
+package ru.aplana.autotests.steps;
+
+import io.qameta.allure.Step;
+import ru.aplana.autotests.pages.MainPage;
+
+public class MainSteps {
+
+    MainPage mainPage = new MainPage();
+
+    @Step("выполнен поиск товара {0}")
+    public void stepSearchProduct(String productName){
+        mainPage.searchProduct(productName);
+    }
+    @Step("Закрыто сообщение про cookies")
+    public void stepCloseCookies(){
+        mainPage.closeCookies();
+    }
+    @Step("Выполнен переход в корзину")
+    public void stepGoToCart(){
+        mainPage.goToCart();
+    }
+}
